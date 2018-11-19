ee70c6dadd098149ffc31fc66b90c3471b6ff87e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-19 17:08:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     binding.pry
-    if params[:username]
+    if params[:username]&.length > 0
 
     else
       redirect '/failure'
